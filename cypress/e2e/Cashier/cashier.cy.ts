2b3e3d9ba47303e4399e13bfc37ad490680c2e1a
EzzAddinKukhun <ezkukhun2000@gmail.com>
2025-05-12 21:35:16 +0300

    cashier page test cases

--- cypress/e2e/Cashier/cashier.cy.ts
+++ cypress/e2e/Cashier/cashier.cy.ts
@@ -17,10 +17,26 @@ describe ('cashier', () => {
     it ('First Login Trial', () => {
         cy.visit('http://localhost:3000/cashier')
     })
-    it ('Checking that username is displayed correctly', () => {
+
+    it ('Checking that username is displayed', () => {
         cy.visit('http://localhost:3000/cashier')
         cy.get(`div[class='pos_userNameWelcome__5ZCu+']`).should('contain', 'Welcome, Ezz Addin')
     })
-    
 
+    it ('Checking that categories imgs are displayed', () => {
+        cy.visit('http://localhost:3000/cashier')
+        cy.get(`img[class='pos_categoryThumbnial__LGzs4']`).each(($img, index) => {
+            cy.wrap($img).should('be.visible').and(($image)=>{
+                const image = $image[0] as HTMLImageElement;
+              expect(image.naturalWidth).to.be.greaterThan(0, `Image at index ${index} is loaded`);
+            });
+        });
+    })
+
+    it.only ('Checking that displayed products are matched with the category selected', () => {
+        cy.visit('http://localhost:3000/cashier')
+        cy.get(`div[class='slick-slide slick-active']`).should('have.length', 5)
+
+    })
+    
 })
